c1abc6c10020210cd9c87b64658d0bea946b8ae6
Sddhrtha <26519057+Sddhrtha@users.noreply.github.com>
2024-10-02 15:57:49 +0530

    removed dotenv

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { AuthProvider } from './context/authProvider';
-require('dotenv').config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
